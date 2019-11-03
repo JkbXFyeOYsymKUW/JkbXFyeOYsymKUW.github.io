7ed62e05a1d9a15bdee847912d7c2e9937724711
JkbXFyeOYsymKUW <57329758+JkbXFyeOYsymKUW@users.noreply.github.com>
2019-11-03 16:12:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-function replaceUrls(text) {
-	var http = text.indexOf("http://");
-	var space = text.indexOf(".me ", http);
-	
-	if (space != -1) { 
-		var url = text.slice(http, space-1);
-		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
-	} 
-	
-	else {
-		return text
-	}
-}
-
-Typer.speed=3;
-Typer.file="jkbxfyeoysymkuw.txt"; // add your own name here
-Typer.init();
- 
-var timer = setInterval("t();", 30);
-function t() {
-	Typer.addText({"keyCode": 123748});
-	
-	if (Typer.index > Typer.text.length) {
-		clearInterval(timer);
-	}
-}
 
 var Typer={
 	text: null,
@@ -108,4 +82,33 @@ var Typer={
 		else
 			this.write("|"); // else write it
 	}
+	
+	function replaceUrls(text) {
+	var http = text.indexOf("http://");
+	var space = text.indexOf(".me ", http);
+	
+	if (space != -1) { 
+		var url = text.slice(http, space-1);
+		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
+	} 
+	
+	else {
+		return text
+	}
+}
+
+Typer.speed=3;
+Typer.file="jkbxfyeoysymkuw.txt"; // add your own name here
+Typer.init();
+ 
+var timer = setInterval("t();", 30);
+function t() {
+	Typer.addText({"keyCode": 123748});
+	
+	if (Typer.index > Typer.text.length) {
+		clearInterval(timer);
+	}
+}
+
+	
 }
